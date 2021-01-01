c1a46ab565cf3cba5fa1976582e4ddf123df90aa
NotKed <kyleedwards031003@gmail.com>
2021-01-01 21:41:56 +0000

    initial commit

---
+++ Countdowns/CountdownsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownsApp.swift
+//  Countdowns
+//
+//  Created by Kyle Edwards on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountdownsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
