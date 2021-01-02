1c97c657f8c274923114179be6c5de849680d6d0
NotKed <kyleedwards031003@gmail.com>
2021-01-02 03:39:12 +0000

    basic visual layout

--- Countdowns/ContentView.swift
+++ Countdowns/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let countdowns: [[String: String]] = [
+        [
+            "name":"Birthday",
+            "visible": "true",
+            "expanded": "false"
+        ],
+        [
+            "name":"Christmas",
+            "visible": "true",
+            "expanded": "false"
+        ]
+    ]
+    
+    let date = Date()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack() {
+            Color.black
+                .ignoresSafeArea()
+            VStack() {
+                Text("COUNTDOWN")
+                    .font(.custom("Menlo:", size: 50.0))
+                    .foregroundColor(.white)
+                    .padding(30)
+                Spacer()
+            }
+            VStack() {
+                ForEach(0..<countdowns.count) { countdown in
+                    if countdowns[countdown]["visible"] == "true" {
+                        Button(action: {
+                            print("hey")
+                        }, label: {
+                            Text("\(countdowns[countdown]["name"]!)     ↓")
+                                .foregroundColor(.white)
+                                .font(.custom("Menlo", size: 35.0))
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                                .padding()
+                            
+                        })
+                        Divider()
+                            .background(Color.white).padding(20).frame(width: 150, height: 20)
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -18,4 +61,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func log(_ text: String) -> EmptyView {
+    print("** \(text)")
+    return EmptyView()
 }
